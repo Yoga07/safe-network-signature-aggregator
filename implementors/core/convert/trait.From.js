c4c9a8eec18e0d914d2e5b7e7656965bb8edd8d6
S-Coyle <S-Coyle@users.noreply.github.com>
2021-01-14 11:06:28 +0000

    deploy: fcfe55fd6ae03f34116c8e858ff68b77aa766370

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["bls_signature_aggregator"] = [{"text":"impl From&lt;Box&lt;ErrorKind&gt;&gt; for AccumulationError","synthetic":false,"types":[]},{"text":"impl From&lt;Error&gt; for AccumulationError","synthetic":false,"types":[]}];
+implementors["bls_signature_aggregator"] = [{"text":"impl From&lt;Box&lt;ErrorKind, Global&gt;&gt; for AccumulationError","synthetic":false,"types":[]},{"text":"impl From&lt;Error&gt; for AccumulationError","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
