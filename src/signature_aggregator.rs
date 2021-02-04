0c0b7ab96bfae28381109ce8ba83a13483db54ee
Adam Cigánek <adam.ciganek@gmail.com>
2021-02-04 10:23:01 +0100

    feat!: make the crate serialization format agnostic + remove fake clock
    BREAKING CHANGES: The `add` method now takes the payload as `&[u8]` instead of `T: Serialize` which separates the serialization concern out of this crate (single responsibility principle) and allows to use any serialization format, not just bincode. Also remove the `mock_timer` feature and the support for fake clock.

--- src/signature_aggregator.rs
+++ src/signature_aggregator.rs
@@ -9,32 +9,18 @@
 
 use super::proof::{Proof, ProofShare};
 use err_derive::Error;
-use serde::Serialize;
-use std::time::Duration;
-use std::{collections::HashMap, fmt::Debug};
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    time::{Duration, Instant},
+};
 use threshold_crypto as bls;
-
-#[cfg(feature = "mock_timer")]
-use sn_fake_clock::FakeClock as Instant;
-#[cfg(not(feature = "mock_timer"))]
-use std::time::Instant;
-
-/// SHA3-256 hash digest.
-pub type Digest256 = [u8; 32];
-
-/// SHA3-256 hash function.
-pub fn sha3_256(input: &[u8]) -> Digest256 {
-    use tiny_keccak::{Hasher, Sha3};
-
-    let mut hasher = Sha3::v256();
-    let mut output = Digest256::default();
-    hasher.update(input);
-    hasher.finalize(&mut output);
-    output
-}
+use tiny_keccak::{Hasher, Sha3};
 
 /// Default duration since their last modification after which all unaccumulated entries expire.
 pub const DEFAULT_EXPIRATION: Duration = Duration::from_secs(120);
+
+type Digest256 = [u8; 32];
 
 /// Accumulator for signature shares for arbitrary payloads.
 ///
@@ -42,18 +28,6 @@ pub const DEFAULT_EXPIRATION: Duration = Duration::from_secs(120);
 /// of them are collected. At that point it combines them into a full BLS signature of the given
 /// payload. It also automatically rejects invalid signature shares and expires entries that did not
 /// collect enough signature shares within a given time.
-///
-/// The accumulated payload needs to implement `Serialize` which is used in two ways:
-///
-/// 1. to calculate a cryptographic hash of the payload which is then used to compute the
-///    accumulation key.
-///    This means that two signature shares with payloads that serialize into the same byte sequence
-///    are accumulated together.
-/// 2. to verify the signature share - the serialized payload is what is passed to the `verify`
-///    function.
-///
-/// The serialization is performed using `bincode::serialize` which also needs to be used to
-/// create the signature share.
 ///
 /// This accumulator also handles the case when the same payload is signed with a signature share
 /// corresponding to a different BLS public key. In that case, the payloads will be accumulated
@@ -80,8 +54,8 @@ impl SignatureAggregator {
     }
 
     /// Add new share into the accumulator. If enough valid signature shares were collected, returns
-    /// the payload and its corresponding `Proof` (signature + public key). Otherwise returns error
-    /// which details why the accumulation did not succeed yet.
+    /// its `Proof` (signature + public key). Otherwise returns error which details why the
+    /// accumulation did not succeed yet.
     ///
     /// Note: returned `AccumulationError::NotEnoughShares` does not indicate a failure. It simply
     /// means more shares still need to be added for that particular payload. Similarly,
@@ -90,44 +64,34 @@ impl SignatureAggregator {
     /// still be useful perhaps for debugging). The other error variants, however, indicate
     /// failures and should be treated a such. See [AccumulationError]
     /// for more info.
-    ///
-    /// Note: the `signature_share` field in the `proof_share` must be created by serializing the
-    /// `payload` with `bincode::serialize` and signing the resulting bytes. Other serialization
-    /// formats are not currently supported.
-    pub fn add<T>(
+    pub fn add(
         &mut self,
-        payload: T,
+        payload: &[u8],
         proof_share: ProofShare,
-    ) -> Result<(T, Proof), AccumulationError>
-    where
-        T: Serialize,
-    {
+    ) -> Result<Proof, AccumulationError> {
         self.remove_expired();
 
-        let mut bytes = bincode::serialize(&payload)?;
-
-        if !proof_share.verify(&bytes) {
+        if !proof_share.verify(payload) {
             return Err(AccumulationError::InvalidShare);
         }
 
         // Use the hash of the payload + the public key as the key in the map to avoid mixing
         // entries that have the same payload but are signed using different keys.
         let public_key = proof_share.public_key_set.public_key();
-        bytes.extend_from_slice(&public_key.to_bytes());
-        let hash = sha3_256(&bytes);
+
+        let mut hasher = Sha3::v256();
+        let mut hash = Digest256::default();
+        hasher.update(payload);
+        hasher.update(&public_key.to_bytes());
+        hasher.finalize(&mut hash);
 
         self.map
             .entry(hash)
             .or_insert_with(State::new)
-            .add(payload, proof_share)
-            .map(|(payload, signature)| {
-                (
-                    payload,
-                    Proof {
-                        public_key,
-                        signature,
-                    },
-                )
+            .add(proof_share)
+            .map(|signature| Proof {
+                public_key,
+                signature,
             })
     }
 
@@ -155,12 +119,6 @@ pub enum AccumulationError {
     /// accumulation might still succeed.
     #[error(display = "signature share is invalid")]
     InvalidShare,
-    /// The payload failed to be serialised and can't be inserted into the accumulator. This doesn't
-    /// affect other entries already present and the accumulator can still be used normally
-    /// afterwards. In practice, this error should never happen unless the host machine runs out of
-    /// memory or a similar catastrophic failure.
-    #[error(display = "failed to serialise payload: {}", _0)]
-    Serialise(#[error(source)] bincode::Error),
     /// The signature combination failed even though there are enough valid signature shares. This
     /// should probably never happen.
     #[error(display = "failed to combine signature shares: {}", _0)]
@@ -180,11 +138,7 @@ impl State {
         }
     }
 
-    fn add<T>(
-        &mut self,
-        payload: T,
-        proof_share: ProofShare,
-    ) -> Result<(T, bls::Signature), AccumulationError> {
+    fn add(&mut self, proof_share: ProofShare) -> Result<bls::Signature, AccumulationError> {
         if self
             .shares
             .insert(proof_share.index, proof_share.signature_share)
@@ -202,7 +156,7 @@ impl State {
                 .combine_signatures(self.shares.iter().map(|(&index, share)| (index, share)))?;
             self.shares.clear();
 
-            Ok((payload, signature))
+            Ok(signature)
         } else {
             Err(AccumulationError::NotEnoughShares)
         }
@@ -222,13 +176,13 @@ mod tests {
         let sk_set = bls::SecretKeySet::random(threshold, &mut rng);
 
         let mut accumulator = SignatureAggregator::default();
-        let payload = "hello".to_string();
+        let payload = b"hello";
 
         // Not enough shares yet
         for index in 0..threshold {
-            let proof_share = create_proof_share(&sk_set, index, &payload);
+            let proof_share = create_proof_share(&sk_set, index, payload);
             println!("{:?}", proof_share);
-            let result = accumulator.add(payload.clone(), proof_share);
+            let result = accumulator.add(payload, proof_share);
 
             match result {
                 Err(AccumulationError::NotEnoughShares) => (),
@@ -237,14 +191,13 @@ mod tests {
         }
 
         // Enough shares now
-        let proof_share = create_proof_share(&sk_set, threshold, &payload);
-        let (accumulated_payload, proof) = accumulator.add(payload.clone(), proof_share).unwrap();
+        let proof_share = create_proof_share(&sk_set, threshold, payload);
+        let proof = accumulator.add(payload, proof_share).unwrap();
 
-        assert_eq!(accumulated_payload, payload);
-        assert!(proof.verify(&bincode::serialize(&accumulated_payload).unwrap()));
+        assert!(proof.verify(payload));
 
         // Extra shares start another round
-        let proof_share = create_proof_share(&sk_set, threshold + 1, &payload);
+        let proof_share = create_proof_share(&sk_set, threshold + 1, payload);
         let result = accumulator.add(payload, proof_share);
 
         match result {
@@ -260,17 +213,17 @@ mod tests {
         let sk_set = bls::SecretKeySet::random(threshold, &mut rng);
 
         let mut accumulator = SignatureAggregator::new();
-        let payload = "good".to_string();
+        let payload = b"good";
 
         // First insert less than threshold + 1 valid shares.
         for index in 0..threshold {
-            let proof_share = create_proof_share(&sk_set, index, &payload);
-            let _ = accumulator.add(payload.clone(), proof_share);
+            let proof_share = create_proof_share(&sk_set, index, payload);
+            let _ = accumulator.add(payload, proof_share);
         }
 
         // Then try to insert invalid share.
-        let invalid_proof_share = create_proof_share(&sk_set, threshold, &"bad".to_string());
-        let result = accumulator.add(payload.clone(), invalid_proof_share);
+        let invalid_proof_share = create_proof_share(&sk_set, threshold, b"bad");
+        let result = accumulator.add(payload, invalid_proof_share);
 
         match result {
             Err(AccumulationError::InvalidShare) => (),
@@ -279,9 +232,9 @@ mod tests {
 
         // The invalid share doesn't spoil the accumulation - we can still accumulate once enough
         // valid shares are inserted.
-        let proof_share = create_proof_share(&sk_set, threshold + 1, &payload);
-        let (accumulated_payload, proof) = accumulator.add(payload, proof_share).unwrap();
-        assert!(proof.verify(&bincode::serialize(&accumulated_payload).unwrap()))
+        let proof_share = create_proof_share(&sk_set, threshold + 1, payload);
+        let proof = accumulator.add(payload, proof_share).unwrap();
+        assert!(proof.verify(payload))
     }
 
     #[test]
@@ -290,18 +243,18 @@ mod tests {
         let threshold = 3;
         let sk_set = bls::SecretKeySet::random(threshold, &mut rng);
 
-        let mut accumulator = SignatureAggregator::with_expiration(Duration::from_secs(3));
-        let payload = "hello".to_string();
+        let mut accumulator = SignatureAggregator::with_expiration(Duration::from_millis(500));
+        let payload = b"hello";
 
         for index in 0..threshold {
-            let proof_share = create_proof_share(&sk_set, index, &payload);
-            let _ = accumulator.add(payload.clone(), proof_share);
+            let proof_share = create_proof_share(&sk_set, index, payload);
+            let _ = accumulator.add(payload, proof_share);
         }
 
-        sleep(Duration::from_secs(5));
+        sleep(Duration::from_secs(1));
 
         // Adding another share does nothing now, because the previous shares expired.
-        let proof_share = create_proof_share(&sk_set, threshold, &payload);
+        let proof_share = create_proof_share(&sk_set, threshold, payload);
         let result = accumulator.add(payload, proof_share);
 
         match result {
@@ -318,43 +271,33 @@ mod tests {
 
         let mut accumulator = SignatureAggregator::new();
 
-        let payload = "hello".to_string();
+        let payload = b"hello";
 
         // round 1
 
         for index in 0..threshold {
-            let proof_share = create_proof_share(&sk_set, index, &payload);
-            assert!(accumulator.add(payload.clone(), proof_share).is_err());
+            let proof_share = create_proof_share(&sk_set, index, payload);
+            assert!(accumulator.add(payload, proof_share).is_err());
         }
 
-        let proof_share = create_proof_share(&sk_set, threshold, &payload);
-        assert!(accumulator.add(payload.clone(), proof_share).is_ok());
+        let proof_share = create_proof_share(&sk_set, threshold, payload);
+        assert!(accumulator.add(payload, proof_share).is_ok());
 
         // round 2
 
         let offset = 2;
 
         for index in offset..(threshold + offset) {
-            let proof_share = create_proof_share(&sk_set, index, &payload);
-            assert!(accumulator.add(payload.clone(), proof_share).is_err());
+            let proof_share = create_proof_share(&sk_set, index, payload);
+            assert!(accumulator.add(payload, proof_share).is_err());
         }
 
-        let proof_share = create_proof_share(&sk_set, threshold + offset + 1, &payload);
+        let proof_share = create_proof_share(&sk_set, threshold + offset + 1, payload);
         assert!(accumulator.add(payload, proof_share).is_ok());
     }
 
-    fn create_proof_share<T: Serialize>(
-        sk_set: &bls::SecretKeySet,
-        index: usize,
-        payload: &T,
-    ) -> ProofShare {
+    fn create_proof_share(sk_set: &bls::SecretKeySet, index: usize, payload: &[u8]) -> ProofShare {
         let sk_share = sk_set.secret_key_share(index);
-
-        ProofShare::new(
-            sk_set.public_keys(),
-            index,
-            &sk_share,
-            &bincode::serialize(&payload).unwrap(),
-        )
+        ProofShare::new(sk_set.public_keys(), index, &sk_share, &payload)
     }
 }
