1a72486a47d98ef605ee50213e2f0b9db0756559
S-Coyle <S-Coyle@users.noreply.github.com>
2021-06-01 10:11:46 +0000

    deploy: 3c2e1d950beaea80eda3e54b6a22ba46a72aeb68

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["bls_signature_aggregator"];
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["bls_signature_aggregator"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["bls_signature_aggregator"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"bls_signature_aggregator/signature_aggregator/enum.Error.html\" title=\"enum bls_signature_aggregator::signature_aggregator::Error\">Error</a>","synthetic":false,"types":["bls_signature_aggregator::signature_aggregator::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
